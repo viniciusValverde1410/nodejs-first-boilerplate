dcd4a6fae653686c825a5b399cd9b21ac82f9af0
vinicius.valverde@aluno.senai.br <vinicius.valverde@aluno.senai.br>
2024-10-24 16:53:48 -0300

    feat: create get method

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -4,5 +4,15 @@ const usuariosRoutes = Router();
 
 let usuarios = []
 
+usuariosRoutes.get("/", (req, res) => {
+    return res.status(200).json({
+        message:
+        usuarios.length == 0
+        ? "Não há usuários cadastrados"
+        : `Total de usuários cadastrados: ${usuarios.length}`,
+        usuarios
+    });
+})
+
 
 export default usuariosRoutes;
